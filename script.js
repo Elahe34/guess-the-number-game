cfbf11dec1746a8a4034386e49778f173895a1d0
Elahe <soltanielahe34@gamil.com>
2025-04-05 11:25:07 +0330

    Add scoring system and random number generation

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-document.querySelector('.check').addEventListener('click' , function(){
-   const guess =Number(document.querySelector('.guess').value)
-   console.log(guess,typeof guess)
-   if(!guess){
-    document.querySelector('.message').textContent = 'No Number 👀'
+const guessNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = guessNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number 👀";
+  } else if (guess === guessNumber) {
+    document.querySelector(".message").textContent = "Excelent to you ";
+  } else if (guess > guessNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "to high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost ";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < guessNumber) {
+   if (score > 1) {
+     document.querySelector(".message").textContent = "to low";
+     score--;
+     document.querySelector(".score").textContent = score;
+   } else {
+     document.querySelector(".message").textContent = "you lost ";
+     document.querySelector(".score").textContent = 0;
    }
-})
+ }
+});
