e483ba816825370baadb66ccebefb228e3cbb8ec
Elahe <soltanielahe34@gamil.com>
2025-04-06 10:00:37 +0330

    finish the game functionality and set comment and cleaning the codes

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-const guessNumber = Math.trunc(Math.random() * 20) + 1;
+let guessNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = guessNumber;
-
+let highscore = Number(localStorage.getItem('highscore'))||0;
+document.querySelector('.highscore').textContent = highscore
+const displayMessage = function(message){
+  document.querySelector(".message").textContent= message
+}
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
+  // when dont have input
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number 👀";
-  } else if (guess === guessNumber) {
-    document.querySelector(".message").textContent = "Excelent to you ";
-  } else if (guess > guessNumber) {
+    displayMessage("No Number 👀");
+  }
+  // when player win
+   else if (guess === guessNumber) {
+    document.querySelector(".number").textContent = guessNumber;
+    displayMessage("Wow you won 😍");
+    document.querySelector('body').style.backgroundColor ='green'
+    document.querySelector('.number').style.color = 'green'
+    document.querySelector('.number').style.fontSize ='3em'
+    if(score > highscore){
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore
+      localStorage.setItem('highscore',highscore)
+    }
+  }
+  // when the guess is wrong
+  else if(guess !== guessNumber){
     if (score > 1) {
-      document.querySelector(".message").textContent = "to high";
+      displayMessage(guess > guessNumber ? "to high 📉" : "to low 📈");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "you lost ";
+      displayMessage("you lost 🥺 ");
+      document.querySelector('body').style.backgroundColor ='red'
+      document.querySelector(".number").textContent = guessNumber;
+      document.querySelector(".number").style.color = 'red';
       document.querySelector(".score").textContent = 0;
     }
-  } else if (guess < guessNumber) {
-   if (score > 1) {
-     document.querySelector(".message").textContent = "to low";
-     score--;
-     document.querySelector(".score").textContent = score;
-   } else {
-     document.querySelector(".message").textContent = "you lost ";
-     document.querySelector(".score").textContent = 0;
-   }
- }
-});
+  }})
+//play again button functionality
+document.querySelector('.btn-again').addEventListener("click",function(){
+  score=20
+  guessNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".score").textContent = score;
+  displayMessage("start guessing...");
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor ='rgb(56, 83, 56)'
+  document.querySelector('.number').style.color = 'rgb(56, 83, 56)'
+  document.querySelector('.number').style.fontSize ='2em'
+  document.querySelector(".number").textContent = '?';
+
+
+})
